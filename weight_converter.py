fe985b25b794a783cdf16e3924b188a22837744e
Fernando Curiel <fcuriel66@mac.com>
2025-08-28 09:39:54 +0200

    Added docstrings

--- weight_converter.py
+++ weight_converter.py
@@ -1,4 +1,9 @@
 
 def grams_to_kilograms(grams):
+    """
+    Converts grams to kilograms using the definition of kilograms
+    :param grams:
+    :return: kilograms
+    """
     kilograms = grams / 1000
     return kilograms
